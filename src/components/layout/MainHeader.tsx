ba257b1ccb4e21771d32914100de8338beae0fb4
Piotr Regulski <bulu901@gmail.com>
2024-05-26 17:59:49 +0200

    added social button

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -6,38 +6,48 @@ import Logo from "./Logo";
 import { useState, useCallback } from "react";
 import HamburgerIcon from "@/components/layout/HamburgerIcon";
 import Link from "next/link";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { BsFillTelephoneForwardFill } from "react-icons/bs";
 
-import { usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const MainHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = useCallback(() => setIsOpen(!isOpen), [isOpen]);
   const currentPath = usePathname();
 
-  console.log(usePathname);
+ 
   const getLinkClassName = (path: string): string => {
-   
-   const formattedCurrentPath = currentPath.replace(/^\//, '');
-    return (!formattedCurrentPath || !formattedCurrentPath.startsWith(path))
-    
-      ?  "px-2 mx-5  text-gray-900 font-bold border-y-2 hover:border-y-2 hover:border-amber-400 hover:shadow-lg"
-      :" px-2 mx-5  border-y-2 border-amber-400 shadow-lg text-gray-900 font-bold";
+    const formattedCurrentPath = currentPath.replace(/^\//, "");
+    return !formattedCurrentPath || !formattedCurrentPath.startsWith(path)
+      ? "px-2 mx-5  text-gray-900 font-bold border-y-2 hover:border-y-2 hover:border-amber-400 hover:shadow-lg"
+      : " px-2 mx-5  border-y-2 border-amber-400 shadow-lg text-gray-900 font-bold";
   };
   return (
     <div className="relative">
       <header
         className={`${CormorantInfant.className}flex flex-col justify-center bg-primary py-1 px-1`}
       >
+     
         <div className="flex justify-between">
           <div className="">
             <button onClick={toggleMenu} className="w-15 h-15 mt-3 ml-1 ">
               <HamburgerIcon />
             </button>
           </div>
-          <div className="fixed sm:bottom-24  bottom-4 right-4 z-20">
-            <button className="mt-8 inline-flex items-center justify-center rounded-full bg-green-600 py-3 px-6 font-dm text-xl font-medium text-white shadow-xl shadow-green-400/75 transition-transform duration-200 ease-in-out hover:scale-[1.02]">
-              <Link href="https://booksy.com/"> Zarezerwuj Wizytę</Link>
-            </button>
+
+          <div className=" fixed sm:bottom-24  bottom-4 right-1 z-20">
+            <div className="flex flex-col">
+            <button className=" fixed top-5 right-3 w-max rounded-full bg-green-600 py-2 px-2 font-dm text-xl font-medium text-white shadow-xl shadow-green-400/75 transition-transform duration-200 ease-in-out hover:scale-[1.02]">
+                <Link href="tel:885987321 "><BsFillTelephoneForwardFill  className="text-right"/></Link>
+              </button>
+              <button className=" fixed top-20 right-3 w-max rounded-full bg-green-600 py-2 px-2 font-dm text-xl font-medium text-white shadow-xl shadow-green-400/75 transition-transform duration-200 ease-in-out hover:scale-[1.02]">
+                <Link href="https://instagram.com/"><FaSquareInstagram className="text-right"/></Link>
+              </button>
+              <button className="mt-8 inline-flex items-center justify-center rounded-full bg-green-600 py-3 px-6 font-dm text-xl font-medium text-white shadow-xl shadow-green-400/75 transition-transform duration-200 ease-in-out hover:scale-[1.02]">
+                <Link href="https://booksy.com/"> Zarezerwuj Wizytę</Link>
+              </button>
+            </div>
           </div>
         </div>
 
@@ -46,13 +56,13 @@ const MainHeader = () => {
             <Logo />
           </Link>
         </div>
-
+        
         <div className=" flex  flex-wrap w-full justify-evenly   py-4 px-4">
           <nav
             id="big-Screen-nav"
             className=" sm:flex justify-center  text-xl pt-6 hidden"
           >
-            <Link href="/" className={getLinkClassName('/')}>
+            <Link href="/" className={getLinkClassName("/")}>
               Strona główna
             </Link>
             <Link href="about" className={getLinkClassName("about")}>
