c35d2a5962b69135b2e02e4fa3f68f4dffb3aba6
Piotr Regulski <bulu901@gmail.com>
2024-06-11 19:01:02 +0200

    Appearance styles changed

--- src/components/layout/CardArticle.tsx
+++ src/components/layout/CardArticle.tsx
@@ -7,7 +7,7 @@ interface CardArticleProps {
 const CardArticle: React.FC<CardArticleProps> = ({ children }) => {
   return (
     <div className="grid justify-items-center content-center my-2 mx-2 sm:my-2 sm:mx-2 bg-primary/45 shadow-xl rounded border-4 border-white  shadow-gray-400/75 min-h-1/3 sm:min-h-max ">
-      <p className="md:text-xl md:text-justify text-center px-6 font-light md:mx-24 mx-1 my-6  text-gray-950 sm:text-2xl">
+      <p className="  text-xl font-[Lato] md:px-10 md:mr-12 leading-loose text-pretty text-gray-900/70">
         {children}
       </p>
     </div>
