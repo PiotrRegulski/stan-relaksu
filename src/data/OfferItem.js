47b80daec75611885fda88f842fb4088ffc89ea6
Piotr Regulski <bulu901@gmail.com>
2024-06-11 18:58:55 +0200

    new texts added to the adventages table

--- src/data/OfferItem.js
+++ src/data/OfferItem.js
@@ -2,26 +2,101 @@ export const OfferItem = [
   {
     id: "Igłoterapia",
     slug: "igloterapia",
-    title: "Igłoterapia estetyczna twarzy z pinezkowaniem 90 min",
+    title: "Igłoterapia estetyczna twarzy",
     image: "offer1.jpg",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae perferendis non, tempora, a saepe ratione architecto deleniti laboriosam quaerat quisquam recusandae ut adipisci iure autem idaliquid cupiditate enim similique.Lorem ipsum dolor sit ametconsectetur adipisicing elit. Molestiae perferendis non, tempora, asaepe ratione architecto deleniti laboriosam quaerat quisquamrecusandae ut adipisci iure autem id alcupiditate enimsimilique.",
+    contentFirst:
+      "Igłoterapia estetyczna twarzy z pinezkowaniem to zaawansowany zabieg, który może odmienić wygląd Twojej skóry. Pozwól mi opowiedzieć Ci o tym fascynującym procesie:",
+    advantages: [
+      {
+        title: "Spłycenie zmarszczek",
+        description:
+          "Igłoterapia stymuluje komórki skóry, pobudzając produkcję kolagenu i elastyny. To naturalne białka sprawiają, że skóra jest jędrna i elastyczna. Dzięki temu zmarszczki zostają spłycone, a Twoja twarz wygląda młodziej.",
+      },
+      {
+        title: "Unikalne cechy twarzy",
+        description:
+          "Ten zabieg podkreśla Twoje naturalne piękno. Nie chodzi o zmianę Twojego wyglądu, ale o wydobycie tego, co w Tobie najpiękniejsze. To idealne rozwiązanie dla osób, które cenią subtelność i naturalność.",
+      },
+      {
+        title: "Efekt odmłodzenia",
+        description:
+          "Cieniutkie igiełki akupunkturowe są precyzyjnie wprowadzane w mięśnie mimiczne. To głębokie rozluźnienie pomaga przywrócić im elastyczność i jędrność. Efekt? Twoja twarz wygląda młodziej, a skóra staje się promienna.",
+      },
+      {
+        title: "Regularność to klucz",
+        description:
+          "Przy regularnym stosowaniu igłoterapii estetycznej zauważysz większe odżywienie tkanek. Twoja skóra będzie wdzięczna za tę pielęgnację!",
+      },
+    ],
+    summary:
+      "Nie wahaj się – umów się na konsultację i dowiedz się więcej o tym fascynującym zabiegu!",
   },
+
   {
     id: "MasażKobido",
     slug: "masaz-kobido",
     title: "Masaż Kobido z kinesiotapingiem estetycznym",
     image: "offer2.jpg",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae perferendis non, tempora, a saepe ratione architecto deleniti laboriosam quaerat quisquam recusandae ut adipisci iure autem idaliquid cupiditate enim similique.Lorem ipsum dolor sit ametconsectetur adipisicing elit. Molestiae perferendis non, tempora, asaepe ratione architecto deleniti laboriosam quaerat quisquamrecusandae ut adipisci iure autem id alcupiditate enimsimilique.",
+    contentFirst:
+      "Masaż Kobido z kinesiotapingiem estetycznym to prawdziwa perła w świecie pielęgnacji twarzy. Pozwól, że opowiem Ci o tym wyjątkowym zabiegu:",
+    advantages: [
+      {
+        title: "Tradycja japońska:",
+        description:
+          "Kobido to technika masażu, która wywodzi się z Japonii. To niechirurgiczny lifting twarzy, który od wieków zachwyca swoją skutecznością..",
+      },
+      {
+        title: "Głębokie rozluźnienie:",
+        description:
+          "Podczas masażu dochodzi do głębokiego rozluźnienia tkanek, nie tylko warstwy podskórnej, ale także mięśni i powięzi twarzy. To jak reset dla Twojej skóry!",
+      },
+      {
+        title: "Efekt liftingu:",
+        description:
+          "Dzięki stymulacji tkanek masażem, Twoja skóra nabiera jędrności. Rysy twarzy są wymodelowane, a efekt liftingu jest widoczny.",
+      },
+      {
+        title: "Odblokowanie przepływu krążenia limfy",
+        description:
+          "To kluczowe dla zdrowej skóry. Kinesiotaping estetyczny na koniec zabiegu pomaga przedłużyć efekt i utrzymać skórę w świetnej kondycji.",
+      },
+      {
+        title: "Regularność:",
+        description:
+          "Zabieg można powtarzać co 2 tygodnie na początku, a po kilku sesjach przejść do 1 zabiegu miesięcznie. To doskonałe przygotowanie tkanek przed innymi zabiegami medycyny estetycznej",
+      },
+    ],
+    summary:
+      "Nie czekaj – umów się na Masaż Kobido i poczuj magię tej tradycyjnej metody! ",
   },
   {
     id: "MasazPowieziowy ",
     slug: "masaz-powieziowy",
     title: "Masaż powięziowy ",
     image: "offer3.jpg",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae perferendis non, tempora, a saepe ratione architecto deleniti laboriosam quaerat quisquam recusandae ut adipisci iure autem idaliquid cupiditate enim similique.Lorem ipsum dolor sit ametconsectetur adipisicing elit. Molestiae perferendis non, tempora, asaepe ratione architecto deleniti laboriosam quaerat quisquamrecusandae ut adipisci iure autem id alcupiditate enimsimilique.",
+    contentFirst:
+      "Masaż powięziowy to doskonały wybór dla osób, które odczuwają bóle pleców, karku lub stawów obwodowych. Pozwól, że opowiem Ci o tym terapeutycznym zabiegu:",
+      advantages: [
+        {
+          title: "Indywidualne podejście:",
+          description:
+            "Każdy organizm jest inny, dlatego terapia jest dostosowana do Twoich potrzeb. Masaż powięziowy pomaga w rozluźnieniu napięć w ciele.",
+        },
+        {
+          title: "Zwalczanie bólu:",
+          description:
+            "Dzięki masażowi możesz pozbyć się dolegliwości bólowych. To jak reset dla Twojego ciała!",
+        },
+        {
+          title: "Techniki terapeutyczne:",
+          description:
+            "W zależności od Twoich potrzeb, terapeuta może użyć technik terapii manualnej, powięziowej lub masażu tkanek głębokich. Celem jest uzyskanie jak najlepszych efektów.",
+        },
+       
+      ],
+      summary:
+        "Nie czekaj – umów się na masaż powięziowy i poczuj ulgę!",
+    
   },
   {
     id: "TerapiaManualnaBlizn ",
