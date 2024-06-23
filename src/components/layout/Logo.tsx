15a790e45df06625594fdd43d95a8b2816bac86a
Piotr Regulski <bulu901@gmail.com>
2024-06-23 12:12:20 +0200

    added a border at the top of the logo

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
         src="/logo.png"
         width={200}
         height={200}
-        className="justify-center  mx-1 my-1 "
+        className="justify-center  mx-1 my-1 border-t-8 rounded-t-full border-t-red-700/75 pt-4 "
         alt="Screenshots of the dashboard project showing desktop version"
         priority={true}
       />
