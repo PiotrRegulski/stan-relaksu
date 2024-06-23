fc4b90cb860e16f070f97e0158de983b641ea0f2
Piotr Regulski <bulu901@gmail.com>
2024-06-23 13:24:42 +0200

    offer cards have been enlarged

--- src/components/offers/OffersCard.tsx
+++ src/components/offers/OffersCard.tsx
@@ -8,32 +8,31 @@ const OffersCard = () => {
   return (
     <>
       <PageHeader>Co oferuję</PageHeader>
-      <div className="flex justify-center">
-        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 sm:gap-12">
+      <div className="flex justify-center w-full">
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-12 lg:gap-6 w-max ">
           {OfferItem.map((offerItem) => (
             <div
               key={offerItem.id}
-              className="mx-4 my-6 bg-white shadow-xl shadow-gray-400/75 animate-fadeIn rounded border-4 border-white transition-transform duration-200 ease-in-out hover:scale-[1.02] h-min"
+              className="mx-6 my-6 lg:w-auto  bg-white shadow-xl shadow-gray-400/75 animate-fadeIn rounded border-4 border-white transition-transform duration-200 ease-in-out hover:scale-[1.02] h-min"
             >
-              <Link className="  " href={`/offer/${offerItem.slug} `}>
+              <Link className="" href={`/offer/${offerItem.slug} `}>
                 <Image
                   src={`/${offerItem.image}`}
                   alt={offerItem.title}
-                  width={400}
+                  width={550}
                   height={400}
                   className=""
                 />
-                <div className="animate-fadeIn rounded border-4 border-white lg:w-[400px] w-full">
+                <div className="animate-fadeIn rounded border-4 border-white  w-full">
                   <p className="lg:text-xl text-center font-bold md:px-3 md:py-4 py-2 font-[Lato] md:h-24 h-[6rem] mx-3">
                     {offerItem.title}
                   </p>
-                  <p className={`${lato.className} h-72 lg:h-48 text-justify  lg:text-xl lg:px-3 px-2`}>
+                  <p className={`${lato.className} h-48 lg:h-36 text-justify  lg:text-xl lg:px-3 px-2`}>
                     {offerItem.contentFirst}
                   </p>
                 </div>
 
                 <div className="flex justify-center h-12">
-                  {" "}
                   <button className=" flex underline decoration-solid underline-offset-8 font-semibold  text-center px-2 py-3  text-black hover:text-gray-400"> Kliknij i zobacz szczegóły</button>
                 </div>
               </Link>
