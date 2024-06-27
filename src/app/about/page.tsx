a43a5e57a4dcac9f37bacdbff36bf25c6d3f49a5
Piotr Regulski <bulu901@gmail.com>
2024-06-27 21:30:27 +0200

    changed top padding

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <>
     <PageHeader>O mnie</PageHeader>
-    <div id="about" className="relative bg-white overflow-hidden mt-16">
+    <div id="about" className="relative bg-white overflow-hidden mt-3">
       
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
