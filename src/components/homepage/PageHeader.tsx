daf86420a3dc6691adc707ef7f2261cea30d5c80
Piotr Regulski <bulu901@gmail.com>
2024-07-21 14:32:54 +0200

    changing colors adding background

--- src/components/homepage/PageHeader.tsx
+++ src/components/homepage/PageHeader.tsx
@@ -9,10 +9,10 @@ const PageHeader: React.FC<PageHeaderProps> = ({ children,id }) => {
   return (
     <header
       id={id}
-      className="grid grid-cols-1 gap-2 place-items-center md:h-32  bg-gradient-to-l from-teal-400/50 via-teal-500/75 to-teal-400/50 pt-2 mt-1"
+      className="grid grid-cols-1 gap-2 place-items-center md:h-32  bg-gradient-to-r from-teal-300/50 to-teal-300/70 rounded-xl"
       // style={{ backgroundImage: "url('/ofertabg.png')" }}
     >
-      <h1 className="px-4 py-3 md:text-4xl text-xl text-center font-semibold   text-white underline">
+      <h1 className="px-4 py-3 md:text-4xl text-xl text-center font-semibold bg-gradient-to-l from-teal-300/50 bg-opacity-75 rounded-xl  text-black underline">
         {children}
       </h1>
     </header>
