14b0e0d40daec4f17bc4901461d5a26373199ec7
Piotr Regulski <bulu901@gmail.com>
2024-07-22 21:50:10 +0200

    the display location of the book button has been changed

--- src/components/layout/NavbarMobile.tsx
+++ src/components/layout/NavbarMobile.tsx
@@ -60,8 +60,8 @@ const NavbarMobile = () => {
             </Link>
           </button>
         </div>
-        <div className="md:hidden fixed bottom-20 right-2 z-20">
-          <button className="animate-fadeIn mt-8 inline-flex items-center justify-center rounded-full bg-green-600 py-3 px-6  font-dm text-sm font-medium text-white shadow-xl shadow-green-400/75 transition-transform duration-400  hover:scale-[1.02]">
+        <div className="md:hidden fixed bottom-[2em] right-[1em] z-20">
+          <button className="animate-fadeIn mt-8 inline-flex items-center justify-center rounded-full bg-green-600 py-3 px-6  font-dm text-sm font-medium text-white shadow-md shadow-gray-400/75 transition-transform duration-400  hover:scale-[1.02]">
             <Link
               href="https://booksy.com/"
               target="_blank"
