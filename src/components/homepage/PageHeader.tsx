a8c52d3cd852af6fcaf310b3f3942b90560b205b
Piotr Regulski <bulu901@gmail.com>
2024-07-23 18:42:17 +0200

    changed background color to gradient

--- src/components/homepage/PageHeader.tsx
+++ src/components/homepage/PageHeader.tsx
@@ -9,7 +9,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ children,id }) => {
   return (
     <header
       id={id}
-      className="grid grid-cols-1 gap-2 place-items-center md:h-32  bg-gradient-to-r from-teal-300/50 to-teal-300/70 rounded-xl"
+      className="grid grid-cols-1 gap-2 place-items-center md:h-[5em]  bg-gradient-to-r from-teal-700/80 via-teal-500/75 to-teal-400/80 shadow-md shadow-gray-400 my-2"
       // style={{ backgroundImage: "url('/ofertabg.png')" }}
     >
       <h1 className="px-4 py-3 md:text-4xl text-xl text-center font-semibold bg-gradient-to-l from-teal-300/50 bg-opacity-75 rounded-xl  text-black underline">
