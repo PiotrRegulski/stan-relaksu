79823a0800072fea3779680f42ceb507706b286f
Piotr Regulski <bulu901@gmail.com>
2024-07-25 17:54:33 +0200

    changed image size

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import logo  from"./public/logo-stan-relaksu-kolo.png"
+
 const Logo = () => {
   return (
     <div className="grid-element">
@@ -9,7 +9,7 @@ const Logo = () => {
         src="/logo-stan-relaksu-kolo.png"
         width={93}
         height={93}
-        className="bg-white rounded-full shadow-xl"
+        className="bg-mainthemelogo rounded-full shadow-xl"
         alt="Screenshots of the dashboard project showing desktop version"
         priority={true}
       />
