115b32c3271738676e5970e69bd7bc6683075156
Piotr Regulski <bulu901@gmail.com>
2024-09-07 20:09:49 +0200

    added info about production

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,9 +1,11 @@
+import ToHomePage from '@/components/layout/ToHomePage';
 import React from 'react';
 
 const pages = () => {
     return (
-        <div className='h-screen'>
-            <p>privacy</p>
+        <div className='flex justify-center items-center h-96 mx-auto my-auto flex-col'>
+            <p>Dokument w przygotowaniu </p>
+            <ToHomePage>Wróć do strony głównej</ToHomePage>
         </div>
     );
 };
