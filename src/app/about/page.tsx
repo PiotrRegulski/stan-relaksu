d87c1f58cd0e96d888c79ef59f3fab62612dc3cc
Piotr Regulski <bulu901@gmail.com>
2024-09-07 20:08:38 +0200

    changed meta description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import AboutMeMainCard from "@/components/about/AboutMeMainCard";
 import AboutMeLastCard from "@/components/about/AboutMeLastCard";
 export const metadata = {
-  title: "O mnie",
+  title: "Stan Relaksu O mnie",
   description:
     " Przyniosę Ci ulgę w bólu oraz napięciu stosując techniki masażu powięziowego i terapii manualnej. Podpowiem Ci jak możesz pracować ze swoim ciałem poprzez dobrane ćwiczenia czy oddech. Dla poprawy Twojego zdrowia i samopoczucia.",
 };
