c46383a1605484d60c8953904bb7200bf401cd4c
Piotr Regulski <bulu901@gmail.com>
2024-09-07 20:07:37 +0200

    component width link to main page

---
+++ src/components/layout/ToHomePage.tsx
@@ -0,0 +1,23 @@
+
+"use client"
+import React from 'react';
+import { useRouter } from 'next/navigation';
+interface ToHomePageProps{
+    children: React.ReactNode;
+   
+  }
+const ToHomePage: React.FC<ToHomePageProps> = ({children}) => {
+    const router = useRouter();
+    return (
+        <div className="flex justify-center">
+        <button
+          onClick={()=>router.push("/")}
+          className=" flex text-sm md:text-xl underline decoration-solid underline-offset-8 font-semibold  text-center  text-black hover:text-gray-400 "
+        >
+         {children}
+        </button>
+      </div>
+    );
+};
+
+export default ToHomePage;
