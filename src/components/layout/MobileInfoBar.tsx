3887b9ddc2f6873f2dc1adc28aea04f055c28dd1
Piotr Regulski <bulu901@gmail.com>
2024-09-07 20:57:29 +0200

    adding the function of the info bar appearing after scrolling the screen

---
+++ src/components/layout/MobileInfoBar.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { BsFillTelephoneForwardFill } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+import Link from "next/link";
+
+const MobileInfoBar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    isVisible && (
+      <div className="md:hidden fixed grid grid-cols-4 gap-1 bottom-0 z-40 bg-primary w-screen h-[4rem] border-t-2 border-white  ">
+        <button className="animate-fadeIn   text-sm font-medium text-white shadow-lg shadow-gray-400/75 transition-transform duration-400  hover:scale-[1.02]">
+          <Link
+            href="https://booksy.com/pl-pl/257898_stan-relaksu-holistyczny-gabinet-terapii-i-masazu_masaz_3_warszawa#ba_s=dl_1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              Zarezerwuj<br></br> Wizytę
+            </div>
+          </Link>
+        </button>
+        <button className=" md:hidden   w-full  text-white shadow-xl  transition-transform duration-200 ease-in-out hover:scale-[1.09] ">
+          <div className="flex justify-center">
+            {" "}
+            <Link href="tel:885987321 ">
+              <BsFillTelephoneForwardFill className="text-center " />
+            </Link>
+          </div>
+        </button>
+        <button className="  text-white shadow-xl  transition-transform duration-200 ease-in-out hover:scale-[1.09] w-full">
+          <div className="flex justify-center">
+            {" "}
+            <Link
+              href="https://www.instagram.com/stan_relaksu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaSquareInstagram className="text-right" />
+            </Link>
+          </div>
+        </button>
+        <button className="    text-white shadow-xl  transition-transform duration-200 ease-in-out hover:scale-[1.09] w-full">
+          <div className="flex justify-center">
+            {" "}
+            <Link
+              href="https://www.facebook.com/profile.php?id=61560021160720"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook className="text-right" />
+            </Link>
+          </div>
+        </button>
+      </div>
+    )
+  );
+};
+
+export default MobileInfoBar;
--- src/components/layout/NavbarMobile.tsx
+++ src/components/layout/NavbarMobile.tsx
@@ -9,7 +9,7 @@ import { FaSquareInstagram } from "react-icons/fa6";
 import { BsFillTelephoneForwardFill } from "react-icons/bs";
 import { FaFacebook } from "react-icons/fa";
 import MobileNavLinks from "./MobileNavLinks";
-import Logo from "./Logo";
+import MobileInfoBar from "./MobileInfoBar";
 const NavbarMobile = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenuAndScroll = useCallback(() => {
@@ -51,51 +51,7 @@ const NavbarMobile = () => {
         </p>
       </div>
 
-      <div className="md:hidden fixed grid grid-cols-4 gap-1 bottom-0 z-40 bg-primary w-screen h-[4rem] border-t-2 border-white  ">
-        <button className="animate-fadeIn   text-sm font-medium text-white shadow-lg shadow-gray-400/75 transition-transform duration-400  hover:scale-[1.02]">
-          <Link
-            href="https://booksy.com/pl-pl/257898_stan-relaksu-holistyczny-gabinet-terapii-i-masazu_masaz_3_warszawa#ba_s=dl_1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div>
-              Zarezerwuj<br></br> Wizytę
-            </div>
-          </Link>
-        </button>
-        <button className=" md:hidden   w-full  text-white shadow-xl  transition-transform duration-200 ease-in-out hover:scale-[1.09] ">
-          <div className="flex justify-center">
-            {" "}
-            <Link href="tel:885987321 ">
-              <BsFillTelephoneForwardFill className="text-center " />
-            </Link>
-          </div>
-        </button>
-        <button className="  text-white shadow-xl  transition-transform duration-200 ease-in-out hover:scale-[1.09] w-full">
-          <div className="flex justify-center">
-            {" "}
-            <Link
-              href="https://www.instagram.com/stan_relaksu/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaSquareInstagram className="text-right" />
-            </Link>
-          </div>
-        </button>
-        <button className="    text-white shadow-xl  transition-transform duration-200 ease-in-out hover:scale-[1.09] w-full">
-          <div className="flex justify-center">
-            {" "}
-            <Link
-              href="https://www.facebook.com/profile.php?id=61560021160720"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaFacebook className="text-right" />
-            </Link>
-          </div>
-        </button>
-      </div>
+     <MobileInfoBar/>
 
       <div
         className={`${CormorantInfant.className} flex flex-wrap w-full justify-evenly lg:hidden`}
