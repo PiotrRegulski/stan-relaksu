f2dabc0ca32186fb6981533a8e98b8582c2bf610
Piotr Regulski <bulu901@gmail.com>
2024-09-20 21:39:17 +0200

    added navigation disappearance function for id offerta

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -4,12 +4,32 @@ import { CormorantInfant } from "@/components/fonts";
 import Navbar from "./Navbar";
 import NavbarMobile from "./NavbarMobile";
 import InfoBar from "./InfoBar";
-
+import { useEffect, useState } from "react";
 const MainHeader = () => {
+  const [isVisible, setIsVisible] = useState(true);
 
+useEffect(() => {
+  const handleScroll = () => {
+    const offerId = document.getElementById("offer-title");
+    if (offerId) {
+      const offerPosition = offerId.getBoundingClientRect().top;
+      console.log(offerPosition)
+      if (offerPosition-864 > 0) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+  return () => {
+    window.removeEventListener("scroll", handleScroll);
+  };
+}, []);
 
   return (
-    <div className="fixed z-40 top-0   ">
+    isVisible &&( <div className="fixed z-40 top-0   ">
       <InfoBar/>
       <header
         className={`${CormorantInfant.className} flex flex-col justify-center bg-white sm:bg-white `}
@@ -24,7 +44,8 @@ const MainHeader = () => {
           </div>
         </div>
       </header>
-    </div>
+    </div>)
+   
   );
 };
 
