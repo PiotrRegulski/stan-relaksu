11223511d449b38432b8d971c67e88c4ad2e2dc6
Piotr Regulski <bulu901@gmail.com>
2024-09-21 12:16:50 +0200

    changed colors and background

--- src/components/pricelist/AllPriceList.tsx
+++ src/components/pricelist/AllPriceList.tsx
@@ -9,7 +9,7 @@ const AllPriceList = () => {
       {OfferItem.map((item) => (
         <div key={item.id} >
           <Link href={`/offer/${item.slug} `} className="hover:opacity-80">
-            <h2 className="bg-primary text-black text-center   text-sm lg:text-xl border rounded-full mx-auto px-8 py-2 font-bold lg:mt-4">
+            <h2 className="bg-primary text-black text-center   text-sm lg:text-xl  rounded-full mx-auto px-8 py-2 font-bold lg:mt-4">
               {item.title}
             </h2>
           </Link>
@@ -17,7 +17,7 @@ const AllPriceList = () => {
           <ul className="divide-y divide-blue-200 my-3 ">
             {item.prices?.map((price) => (
               <li key={price.id} className="text-sm lg:text-xl mx-12 ">
-                <div className="flex justify-between font-semibold bg-primary bg-opacity-50 px-1 rounded">
+                <div className="flex justify-between font-semibold bg-white bg-opacity-50 px-1 rounded">
                   <div className=""><p className="">{price.priceName}</p></div>
                   <div className=""><p className="">{price.price}</p></div>
                 </div>
