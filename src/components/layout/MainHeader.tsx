63826c3f403090aface3f8b546ceb4714bcaa083
Piotr Regulski <bulu901@gmail.com>
2024-09-21 12:08:38 +0200

    added options for navigation to disappear at appropriate moments

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -5,47 +5,59 @@ import Navbar from "./Navbar";
 import NavbarMobile from "./NavbarMobile";
 import InfoBar from "./InfoBar";
 import { useEffect, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+
+
 const MainHeader = () => {
   const [isVisible, setIsVisible] = useState(true);
-
-useEffect(() => {
-  const handleScroll = () => {
-    const offerId = document.getElementById("offer-title");
-    if (offerId) {
-      const offerPosition = offerId.getBoundingClientRect().top;
-      console.log(offerPosition)
-      if (offerPosition-864 > 0) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    const handleScroll = () => {
+      const offerId =
+        document.getElementById("offer") ||
+        document.getElementById("kontakt-info")||
+        document.getElementById("important-about");
+      if (offerId) {
+        const offerPosition = offerId.getBoundingClientRect().top;
+        
+        setIsVisible(offerPosition > 0);
       }
-    }
-  };
+    };
+    const handleRouteChange = () => {
+      setIsVisible(true);
+    };
 
-  window.addEventListener("scroll", handleScroll);
-  return () => {
-    window.removeEventListener("scroll", handleScroll);
-  };
-}, []);
+    window.addEventListener("scroll", handleScroll);
+    handleScroll(); // Uruchomienie funkcji przy pierwszym renderowaniu
+
+    // Nasłuchiwanie na zmiany trasy
+    handleRouteChange();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [pathname, searchParams]);
 
   return (
-    isVisible &&( <div className="fixed z-40 top-0   ">
-      <InfoBar/>
-      <header
-        className={`${CormorantInfant.className} flex flex-col justify-center bg-white sm:bg-white `}
-      >
-     
-        <NavbarMobile />
-        <div className=" flex lg:gap-12 ">
-          <div
-            className={`${CormorantInfant.className} hidden lg:flex w-screen `}
-          >
-            <Navbar />
+    isVisible && (
+      <div className="fixed z-40 top-0 transition  ">
+        <InfoBar />
+        <header
+          className={`${CormorantInfant.className} flex flex-col justify-center bg-white sm:bg-white `}
+        >
+          <NavbarMobile />
+          <div className=" flex lg:gap-12 ">
+            <div
+              className={`${CormorantInfant.className} hidden lg:flex w-screen `}
+            >
+              <Navbar />
+            </div>
           </div>
-        </div>
-      </header>
-    </div>)
-   
+        </header>
+      </div>
+    )
   );
 };
 
