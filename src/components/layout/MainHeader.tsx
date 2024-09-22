6e7624bd7dae4b51d33255983f638fd21d37750d
Piotr Regulski <bulu901@gmail.com>
2024-09-22 11:06:13 +0200

    added suspense and use callback

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -1,63 +1,59 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState, useCallback,Suspense } from "react";
 import { CormorantInfant } from "@/components/fonts";
 import Navbar from "./Navbar";
 import NavbarMobile from "./NavbarMobile";
 import InfoBar from "./InfoBar";
-import { useEffect, useState } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-
-
 
 const MainHeader = () => {
   const [isVisible, setIsVisible] = useState(true);
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
+  const handleScroll = useCallback(() => {
+    const offerId =
+      document.getElementById("offer-title") ||
+      document.getElementById("kontakt-info") ||
+      document.getElementById("important-about");
+    if (offerId) {
+      const offerPosition = offerId.getBoundingClientRect().top;
+      setIsVisible(offerPosition > 0);
+    }
+  }, []);
+
   useEffect(() => {
-    const handleScroll = () => {
-      const offerId =
-        document.getElementById("offer-title") ||
-        document.getElementById("kontakt-info")||
-        document.getElementById("important-about");
-      if (offerId) {
-        const offerPosition = offerId.getBoundingClientRect().top;
-        
-        setIsVisible(offerPosition > 0);
-      }
-    };
     const handleRouteChange = () => {
       setIsVisible(true);
     };
 
     window.addEventListener("scroll", handleScroll);
-    handleScroll(); // Uruchomienie funkcji przy pierwszym renderowaniu
+    handleScroll(); // Run on first render
 
-    // Nasłuchiwanie na zmiany trasy
+    // Listen for route changes
     handleRouteChange();
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [pathname, searchParams]);
+  }, [handleScroll, pathname, searchParams]);
 
   return (
-    isVisible && (
-      <div className="fixed z-40 top-0 transition  ">
+    <Suspense fallback={<div>Loading...</div>}>
+    {isVisible && (
+      <div className="fixed z-40 top-0 transition">
         <InfoBar />
-        <header
-          className={`${CormorantInfant.className} flex flex-col justify-center bg-white sm:bg-white `}
-        >
+        <header className={`${CormorantInfant.className} flex flex-col justify-center bg-white`}>
           <NavbarMobile />
-          <div className=" flex lg:gap-12 ">
-            <div
-              className={`${CormorantInfant.className} hidden lg:flex w-screen `}
-            >
+          <div className="flex lg:gap-12">
+            <div className={`${CormorantInfant.className} hidden lg:flex w-screen`}>
               <Navbar />
             </div>
           </div>
         </header>
       </div>
-    )
+    )}
+  </Suspense>
   );
 };
 
