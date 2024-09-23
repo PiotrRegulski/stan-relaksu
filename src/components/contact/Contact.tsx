5836139b549b905c798daf346b120cf70544acf7
Piotr Regulski <bulu901@gmail.com>
2024-09-23 21:40:57 +0200

    changed image logo

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 import MapComponent from "./MapComponent";
-
+import Image from "next/image";
 import PageHeader from "../homepage/PageHeader";
 import AdressCard from "./AdressCard";
 import DetailedAddress from "./DetailedAddress";
 import ContactForm from "./ContactForm";
 const Contact = () => {
- 
   return (
     <div className="w-full">
       <PageHeader id={"kontakt"}>Skontaktuj się</PageHeader>
@@ -15,8 +14,11 @@ const Contact = () => {
         className="grid md:grid-cols-1
         gap-4  items-center my-4 lg:my-4 mx-auto lg:mx-6 "
       >
-        <div id="kontakt-info" className="px-2 shadow-xl rounded border-4 bg-white border-white  shadow-gray-400/75 w-full">
-          <div className="text-xl font-bold md:mb-2 py-8 text-center ">
+        <div
+          id="kontakt-info"
+          className="px-2 shadow-xl rounded border-4 bg-secondary border-white  shadow-gray-400/75 w-full"
+        >
+          <div className="text-xl font-bold md:mb-2 py-8 text-center bg-">
             <p className="text-2xl my-8">
               Gabinet mieści się przy <strong>studio jogi Jasna Asana. </strong>
             </p>{" "}
@@ -26,8 +28,18 @@ const Contact = () => {
               Możesz zatroszczyć się o siebie w sposób kompleksowy.
             </p>
           </div>
-
-          <div className="flex md:flex-row flex-col justify-around my-8 py-2 w-full bg-secondary ">
+          <div className="relative w-[30rem] h-[30rem] bg-white">
+            <Image
+              src={"/contact/gabient.jpg"}
+             
+              fill
+              className="shadow-gray-400 shadow-md bg-white object-fill w-[30rem] h-[30rem]"
+              alt="Screenshots of the dashboard project showing desktop version"
+              priority={true}
+             
+            />
+          </div>
+          <div className="flex md:flex-row flex-col justify-around my-8 py-2 w-full bg-white shadow-md shadow-primary ">
             <AdressCard
               title={"Jasna Asana Studio Jogi"}
               href={"https://www.jasnaasana.pl/grafik"}
@@ -38,11 +50,13 @@ const Contact = () => {
             />
             <AdressCard
               title={"Gabient Stan Relaksu"}
-              href={"https://booksy.com/pl-pl/257898_stan-relaksu-holistyczny-gabinet-terapii-i-masazu_masaz_3_warszawa#ba_s=dl_1"}
+              href={
+                "https://booksy.com/pl-pl/257898_stan-relaksu-holistyczny-gabinet-terapii-i-masazu_masaz_3_warszawa#ba_s=dl_1"
+              }
               linkTitle={"Zarezerwuj wizytę"}
               instaHref={"https://www.instagram.com/stan_relaksu/"}
               fbHref={"https://www.facebook.com/profile.php?id=61560021160720"}
-              logoSrc={"/logo-stan-relaksu-kolo-tlo.png"}
+              logoSrc={"/logo-stan-relaksu-kolo.png"}
             />
           </div>
           <div className="flex flex-col-reverse sm:flex-row ">
@@ -54,8 +68,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <ContactForm/>
-       
+        <ContactForm />
       </div>
     </div>
   );
