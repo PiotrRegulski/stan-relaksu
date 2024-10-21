8e3035b42e1476a95402cf7e9bb48f70688556a9
Piotr Regulski <bulu901@gmail.com>
2024-10-21 21:22:33 +0200

    changed text button

--- src/components/about/ButtonOffers.tsx
+++ src/components/about/ButtonOffers.tsx
@@ -8,7 +8,7 @@ const ButtonOffers = () => {
       
         className="hover:shadow-xl shadow-lg shadow-gray-400/75 text-lg text-center lg:text-lg text-white   rounded  hover:border-white  bg-primary hover:bg-teal-400/75 px-6 py-2 font-bold   hover:text-gray-700 duration-300  focus:outline-none focus:shadow-outline hover:underline  "
       >
-       Zobacz moją ofertę
+       Zobacz pełną ofertę
       </Link>
     </div>
   );
