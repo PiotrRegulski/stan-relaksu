bb633399e11fae48a2b73e11f22dc7bb05e04d8a
Piotr Regulski <bulu901@gmail.com>
2024-10-23 20:55:29 +0200

    added robots and openGraph to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,35 @@ import MobileInfoBar from "@/components/layout/MobileInfoBar";
 
 export const metadata: Metadata = {
   title: "Stan Relaksu - Gabinet terapii manualnej ciała i twarzy.",
-  description: " Gabinet Stan Relaksu to terapeutyczne miejsce dla ciała stworzone z myślą o Tobie, osobie która chce o zadbać o swoje zdrowie, zwolnić i poczuć się lepiej.",
+  description:
+    " Gabinet Stan Relaksu to terapeutyczne miejsce dla ciała stworzone z myślą o Tobie, osobie która chce o zadbać o swoje zdrowie, zwolnić i poczuć się lepiej.",
+  robots: {
+    index: true, // Zmieniono z false na true
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  openGraph: {
+    title: "Stan Relaksu - Gabinet terapii manualnej ciała i twarzy.",
+    description: "Gabinet Stan Relaksu to terapeutyczne miejsce dla ciała stworzone z myślą o Tobie, osobie która chce o zadbać o swoje zdrowie, zwolnić i poczuć się lepiej.",
+    url: "https://www.stanrelaksu.com",
+    type: 'website',
+    images: [
+      {
+        url: "https://www.stanrelaksu.com/masaz-relaksacyjny/masaz-relaksacyjny-cover-top.jpg",
+        width: 800,
+        height: 600,
+        alt: "Stan Relaksu - Gabinet terapii manualnej ciała i twarzy",
+      }
+    ],
+  }
 };
 
 export default function RootLayout({
@@ -23,7 +51,7 @@ export default function RootLayout({
         className={`${lato.className} relative antialiased  overflow-y-scroll overflow-x-hidden bg-white max-w-[1920px]    my-1  `}
       >
         <MainHeader />
-        <MobileInfoBar/>
+        <MobileInfoBar />
         <main className="overflow-hidden flex flex-col bg-white shadow-lg shadow-white mx-auto ">
           {" "}
           {children}
