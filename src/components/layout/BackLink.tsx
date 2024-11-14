0eb57d9228a566589e8b4cb62ad8f099906d89d7
Piotr Regulski <bulu901@gmail.com>
2024-11-14 21:01:19 +0100

    changed font-style

--- src/components/layout/BackLink.tsx
+++ src/components/layout/BackLink.tsx
@@ -1,23 +1,22 @@
-
-"use client"
-import React from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Noto } from "../fonts";
 interface PageBackLinkProps {
-    children: React.ReactNode;
-   
-  }
-const BackLink: React.FC<PageBackLinkProps> = ({children}) => {
-    const router = useRouter();
-    return (
-        <div className="flex justify-center">
-        <button
-          onClick={()=>router.push("/offer")}
-          className=" flex text-sm md:text-xl underline decoration-solid underline-offset-8 font-semibold  text-center px-2 py-3 mx-3 my-4 text-black hover:text-gray-400 "
-        >
-         {children}
-        </button>
-      </div>
-    );
+  children: React.ReactNode;
+}
+const BackLink: React.FC<PageBackLinkProps> = ({ children }) => {
+  const router = useRouter();
+  return (
+    <div className="flex justify-center">
+      <button
+        onClick={() => router.push("/offer")}
+        className={`${Noto.className} flex text-sm md:text-lg underline decoration-solid underline-offset-8 font-semibold  text-center px-2 py-3 mx-3 my-4 text-black hover:text-gray-400`}
+      >
+        {children}
+      </button>
+    </div>
+  );
 };
 
 export default BackLink;
