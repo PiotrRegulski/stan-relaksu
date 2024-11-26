118f492c79bae91e7e82a130dba926f516066f88
Piotr Regulski <bulu901@gmail.com>
2024-11-26 20:59:37 +0100

    changed font size

--- src/components/comments/CommentsItem.tsx
+++ src/components/comments/CommentsItem.tsx
@@ -5,15 +5,15 @@ const CommentsItem = (props: any) => {
     <div className={props.className}>
       <div className=" flex flex-col w-full justify-center items-center ">
         <p className="text-center text-sm md:text-lg italic px-3 lg:px-24 xl:px-72 2xl:px-[25rem] py-2 sm:px-24  ">{props.comment}</p>
-        <p className="text-center font-bold "> ~ {props.author}</p>
+        <p className="text-center font-bold text-sm"> ~ {props.author}</p>
       </div>
       <div className="flex lg:justify-end w-full px-2 lg:px-72 mt-8">
-        <p className="text-base ">
+        <p className="text-sm ">
           Usługa: {props.service}
         </p>
       </div>
       <div className="flex lg:justify-end w-full px-2  lg:px-72 mt-1">
-        <p className="text-base">
+        <p className="text-sm">
           Źródło: {props.source}
         </p>
       </div>
