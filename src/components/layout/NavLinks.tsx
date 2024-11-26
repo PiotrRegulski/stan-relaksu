67074e3fdaf038d0c0845527ab86694a507d7592
Piotr Regulski <bulu901@gmail.com>
2024-11-26 18:34:37 +0100

    changed text color

--- src/components/layout/NavLinks.tsx
+++ src/components/layout/NavLinks.tsx
@@ -19,7 +19,7 @@ const NavLinks = () => {
           key={link.href}
           className={`border-b-2 border-primary   h-[6rem] flex justify-center items-center text-black sm:text-sm md:text-md lg:text-lg  md:px-2 font-medium   bg-teal-950 bg-transparent lg:mt-1  ${
             pathname === link.href
-              ? "px-2    mt-1   text-triadgray border-primary/60"
+              ? "px-2 pb-1   text-triadbrown border-primary/20 "
               : "  hover:border-primary/40 hover:opacity-40 hover:shadow-sm hover:underline  "
           }`}
         >
