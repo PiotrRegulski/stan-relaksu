da55993034b7e8883daca155deea39f3033db21d
Piotr Regulski <bulu901@gmail.com>
2024-12-09 21:13:59 +0100

    changed cards size

--- src/components/offers/CardFaceItem.tsx
+++ src/components/offers/CardFaceItem.tsx
@@ -40,10 +40,10 @@ const CardFaceItem = () => {
     {categoryFace.map((offerItem) => (
       <div
         key={offerItem.id}
-        className="h-[32rem]   my-6  md:mx-2 sm:h-[28rem] md:h-[30rem] lg:h-[30rem] xl:h-[30rem] 2xl:h-[42rem]  bg-white shadow-lg shadow-gray-400 animate-fadeIn rounded-xl   transition-transform xl:hover:z-30 duration-500 ease-in-out xl:hover:scale-[1.03]   2xl:w-[30rem]  "
+        className="h-[32rem]   my-6  md:mx-2 sm:h-[21rem] md:h-[30rem] lg:h-[30rem] xl:h-[30rem] 2xl:h-[42rem]  bg-white shadow-lg shadow-gray-400 animate-fadeIn rounded-xl   transition-transform xl:hover:z-30 duration-500 ease-in-out xl:hover:scale-[1.03]   2xl:w-[30rem]  "
       >
         <Link className="" href={`/oferta/${offerItem.slug} `}>
-          <div className="relative h-[32rem] w-[20rem] sm:h-[28rem]    sm:w-[19rem] md:w-[24rem] md:h-[30rem] lg:h-[30rem] lg:min-w-[20rem] xl:h-[30rem] xl:w-[25rem] 2xl:h-[42rem] 2xl:w-[30rem]  ">
+          <div className="relative h-[32rem] w-[20rem] sm:h-[21rem]    sm:w-[19rem] md:w-[24rem] md:h-[30rem] lg:h-[30rem] lg:min-w-[20rem] xl:h-[30rem] xl:w-[25rem] 2xl:h-[42rem] 2xl:w-[30rem]  ">
             <Image
               src={`/${offerItem.image}`}
               alt={offerItem.title}
@@ -51,7 +51,7 @@ const CardFaceItem = () => {
               sizes="(max-width: 600px) 100vw, 50vw"
               className=" object-cover w-full h-full rounded-lg "
             />
-            <div className="absolute bottom-0 left-0 bg-secondary bg-opacity-70   rounded w-full h-1/2 sm:h-1/2 md:h-[48%]">
+            <div className="absolute bottom-0 left-0 bg-secondary bg-opacity-70   rounded w-full h-1/2 sm:h-[55%] md:h-[48%]">
               <div className=" flex flex-col justify-center items-center animate-fadeIn rounded   px-1  w-full  h-3/4 ">
                 <div className="h-1/4 flex mt-2 ">
                   <p
