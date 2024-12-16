277beaf7516953ae4750f1fa773d3de3459b7a54
Piotr Regulski <bulu901@gmail.com>
2024-12-16 21:06:27 +0100

    new site-map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,15 @@
 module.exports = {
-    siteUrl: 'https://stanrelaksu.pl',
-    generateRobotsTxt: true, // (Opcjonalne) generowanie pliku robots.txt
-  }
+  siteUrl: "https://stanrelaksu.pl",
+  generateRobotsTxt: true, // (Opcjonalne) generowanie pliku robots.txt
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/admin/*", "/private/*","/privacy-policy/*","/terms/*"],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "Googlebot", allow: "/" },
+      { userAgent: "Bingbot", allow: "/" },
+    ],
+  },
+};
