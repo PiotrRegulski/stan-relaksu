f25622d740c788cda2cdf6e4393e2e48865ee846
Piotr Regulski <bulu901@gmail.com>
2024-12-30 21:47:43 +0100

    fix image

--- src/components/layout/ImageHeader.tsx
+++ src/components/layout/ImageHeader.tsx
@@ -6,7 +6,7 @@ interface ImageHeaderProps{
 const ImageHeader:React.FC<ImageHeaderProps> = ({children,BgImage}) => {
   return (
     <div>
-      <div className={`container mx-auto ${BgImage} bg-no-repeat bg-cover h-[25rem] sm:h-[18rem] 2xl:h-[35rem]`}>
+      <div className={`container mx-auto ${BgImage} bg-no-repeat bg-cover h-[25rem] sm:h-[18rem] 2xl:h-[35rem] animate-fadeIn`}>
         <div className="w-full h-full flex justify-center items-center ">
           <h1 className="text-7xl text-white">{children}</h1>
         </div>
