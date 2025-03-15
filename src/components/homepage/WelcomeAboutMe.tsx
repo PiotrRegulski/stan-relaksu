fc831df553990c0d98cc871d0b4767c476b768c0
Piotr Regulski <bulu901@gmail.com>
2025-03-15 17:28:06 +0100

    changed animation

--- src/components/homepage/WelcomeAboutMe.tsx
+++ src/components/homepage/WelcomeAboutMe.tsx
@@ -8,7 +8,7 @@ const WelcomeAboutMe = () => {
   return (
     <div id="o-gabinecie" className=" flex w-full flex-col  text-black bg-gradient-to-r from-secondary via-white to-secondary mt-[4rem] lg:mt-[7rem] ">
       <div
-        className="flex flex-col justify-center items-center  mx-6 md:mx-24 lg:mx-24 xl:mx-36 my-6 2xl:my-4 lg:my-5  rounded-lg animate-fadeIn"
+        className="flex flex-col justify-center items-center  mx-6 md:mx-24 lg:mx-24 xl:mx-36 my-6 2xl:my-4 lg:my-5 rounded-lg"
       >
         <h1 className="text-center font-normal text-lg lg:text-xl xl:text-3xl ">
           Stan Relaksu<br></br>Gabinet terapii manualnej i masażu
