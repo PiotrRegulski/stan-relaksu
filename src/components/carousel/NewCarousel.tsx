fd4aef76822885f6c5ac0cd140428455d05d53e0
Piotr Regulski <bulu901@gmail.com>
2025-03-15 20:39:41 +0100

    fix animation

--- src/components/carousel/NewCarousel.tsx
+++ src/components/carousel/NewCarousel.tsx
@@ -119,7 +119,7 @@ const NewCarousel = () => {
         {images.map((image, index) => (
           <div
             key={index}
-            className=" relative h-[500px]  lg:h-[500px] xl:h-[600px]  short-height:h-[300px] animate-slideLeft  "
+            className=" relative h-[500px]  lg:h-[500px] xl:h-[600px]  short-height:h-[300px] animate-fadeIn  "
           >
             <Image
               src={image.src}
