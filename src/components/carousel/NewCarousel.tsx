46169f7a291511180586d57e116101bbe13bbd62
Piotr Regulski <bulu901@gmail.com>
2025-03-16 18:26:54 +0100

    fix slider restore

--- src/components/carousel/NewCarousel.tsx
+++ src/components/carousel/NewCarousel.tsx
@@ -1,6 +1,5 @@
 "use client";
-import dynamic from "next/dynamic";
-
+import Slider from "react-slick";
 import Image from "next/image";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
@@ -9,11 +8,9 @@ import { useRouter } from "next/navigation";
 import ButtonMain from "../layout/ButtonMain";
 import { lato } from "../fonts";
 import { NextArrow, PrevArrow } from "@/components/carousel/Arrows";
-import { useMemo } from "react";
-const Slider = dynamic(() => import("react-slick"), { ssr: false });
+
 interface ImageData {
   src: string;
-  smallImage:string;
   alt: string;
   description: string;
   linkDetails: string;
@@ -23,139 +20,129 @@ interface ImageData {
 
 const NewCarousel = () => {
   const router = useRouter();
-  const settings = useMemo(
-    () => ({
-     
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 6000,
-      arrows: true,
-      nextArrow: <NextArrow />,
-      prevArrow: <PrevArrow />,
-    }),
-    []
-  );
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
 
-  const images: ImageData[] = useMemo(
-    () => [
-      {
-        src: "/carousel/masaz-powieziowy-top.webp",
-        smallImage:"/carousel/masaz-powieziowy-top-small.webp",
-        alt: "Masaż powięziowy",
-        description:
-          "Idealny wybór dla osób, które zmagają się z silnymi bólami i napięciami w obrębie ciała",
-        linkDetails: "oferta/masaz-powieziowy",
-        linkBooksy:
-          "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
-        clName:
-          "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
-      },
-      {
-        src: "/carousel/terapia-manualna-top.webp",
-        smallImage:"/carousel/masaz-powieziowy-top-small.webp",
-        alt: "Terapia manualna",
-        description:
-          "Specjalistyczne techniki mające na celu przywrócenie prawidłowego funkcjonowania układu ruchu, zmniejszenie sztywności i ograniczeń w zakresie ruchomości. ",
-        linkDetails: "oferta/terapia-manualna",
-        linkBooksy:
-          "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
-        clName:
-          "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
-      },
+  const images: ImageData[] = [
+  
+    {
+      src: "/carousel/masaz-powieziowy-top.webp",
+      alt: "Masaż powięziowy",
+      description:
+        "Idealny wybór dla osób, które zmagają się z silnymi bólami i napięciami w obrębie ciała",
+      linkDetails: "oferta/masaz-powieziowy",
+      linkBooksy:
+        "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
+      clName:
+        "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
+    },
+    {
+      src: "/carousel/terapia-manualna-top.webp",
+      alt: "Terapia manualna",
+      description:
+        "Specjalistyczne techniki mające na celu przywrócenie prawidłowego funkcjonowania układu ruchu, zmniejszenie sztywności i ograniczeń w zakresie ruchomości. ",
+      linkDetails: "oferta/terapia-manualna",
+      linkBooksy:
+        "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
+      clName:
+        "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
+    },
 
-      {
-        src: "/carousel/masaz-kobido-top.webp",
-        smallImage:"/carousel/masaz-powieziowy-top-small.webp",
-        alt: "Kobido z kinesiotapingiem limfatycznym",
-        description:
-          "Zdrowszy i promienny wygląd twarzy, spłycenie zmarszczek oraz bruzd mimicznych to jedne z wielu korzyści płynących z rytuału Kobido.",
-        linkDetails: "oferta/masaz-kobido",
-        linkBooksy:
-          "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
-        clName:
-          "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
-      },
-      {
-        src: "/carousel/terapia-manualna-zatok-top.webp",
-        smallImage:"/carousel/masaz-powieziowy-top-small.webp",
-        alt: "Terapia manualna w bólach zatok ",
-        description:
-          "Techniki manualne stosowane podczas terapii pomagają w odblokowaniu dróg oddechowych, poprawiają krążenie krwi oraz zmniejszają napięcie mięśniowe w okolicy twarzy i głowy.",
-        linkDetails: "oferta/terapia-manualna-w-bolach-zatok",
-        linkBooksy:
-          "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
-        clName:
-          "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
-      },
+    {
+      src: "/carousel/masaz-kobido-top.webp",
+      alt: "Kobido z kinesiotapingiem limfatycznym",
+      description:
+        "Zdrowszy i promienny wygląd twarzy, spłycenie zmarszczek oraz bruzd mimicznych to jedne z wielu korzyści płynących z rytuału Kobido.",
+      linkDetails: "oferta/masaz-kobido",
+      linkBooksy:
+        "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
+      clName:
+        "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
+    },
+    {
+      src: "/carousel/terapia-manualna-zatok-top.webp",
+      alt: "Terapia manualna w bólach zatok ",
+      description:
+        "Techniki manualne stosowane podczas terapii pomagają w odblokowaniu dróg oddechowych, poprawiają krążenie krwi oraz zmniejszają napięcie mięśniowe w okolicy twarzy i głowy.",
+      linkDetails: "oferta/terapia-manualna-w-bolach-zatok",
+      linkBooksy:
+        "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
+      clName:
+        "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
+    },
 
-      {
-        src: "/carousel/masaz-tkanek-glebokich-top.webp",
-        smallImage:"/carousel/masaz-powieziowy-top-small.webp",
-        alt: "Masaż tkanek głębokich",
-        description:
-          "Niezwykle skuteczna technika pracy z ciałem, koncentrująca się na rozluźnieniu głębokich warstw mięśni i tkanki powięziowej. ",
-        linkDetails: "oferta/masaz-tkanek-glebokich",
-        linkBooksy:
-          "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
-        clName:
-          "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
-      },
-      {
-        src: "/carousel/masaz-relaksacyjny-top.webp",
-        smallImage:"/carousel/masaz-powieziowy-top-small.webp",
-        alt: "Masaż relaksacyjny Stanu Relaksu",
-        description:
-          "Przenieś się do pełnego stanu relaksu z masażem, który głęboko odpręży Twoje mięśnie i zdejmie napięcie z układu nerwowego, zmniejszając uczucie stresu. ",
-        linkDetails: "oferta/masaz-relaksacyjny-stanu-relaksu",
-        linkBooksy:
-          "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
-        clName:
-          "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
-      },
-    ],
-    []
-  );
+    {
+      src: "/carousel/masaz-tkanek-glebokich-top.webp",
+      alt: "Masaż tkanek głębokich",
+      description:
+        "Niezwykle skuteczna technika pracy z ciałem, koncentrująca się na rozluźnieniu głębokich warstw mięśni i tkanki powięziowej. ",
+      linkDetails: "oferta/masaz-tkanek-glebokich",
+      linkBooksy:
+        "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
+      clName:
+        "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
+    },
+    {
+      src: "/carousel/masaz-relaksacyjny-top.webp",
+      alt: "Masaż relaksacyjny Stanu Relaksu",
+      description:
+        "Przenieś się do pełnego stanu relaksu z masażem, który głęboko odpręży Twoje mięśnie i zdejmie napięcie z układu nerwowego, zmniejszając uczucie stresu. ",
+      linkDetails: "oferta/masaz-relaksacyjny-stanu-relaksu",
+      linkBooksy:
+        "https://booksy.com/pl-pl/257898_stan-relaksu-gabinet-terapii-manualnej-i-masazu_masaz_3_warszawa#ba_s=dl_1",
+      clName:
+        "text-white xl:text-2xl font-serif tracking-widest hover:opacity-70 hover:underline ",
+    },
+  ];
 
   return (
-    <div className="relative h-full lg:h-[540px] xl:h-[610px] short-height:h-[340px] ">
+    <div className="relative h-[540px] lg:h-[540px] xl:h-[610px] short-height:h-[340px]  animate-fadeIn">
+      
       {" "}
       <Slider {...settings}>
         {images.map((image, index) => (
+         
           <div
             key={index}
-            className=" relative h-[400px]  lg:h-[500px] xl:h-[600px]  short-height:h-[300px] lg:animate-fadeIn  w-screen "
+            className=" relative h-[500px]  lg:h-[500px] xl:h-[600px]  short-height:h-[300px]  "
           >
             <Image
               src={image.src}
               alt={image.alt}
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 75vw, 50vw"
-            
               fill
-              priority
               className="object-cover"
-              
-              quality={65}
-            />
-
+              priority
+              quality={65} 
+             />
+           
             <div className="absolute bottom-0  w-full h-full text-center rounded  bg-black bg-opacity-20 z-20 ">
+            
               <div className="flex flex-col justify-end items-center xl:w-full 2xl:w-full h-full">
                 <div className="flex flex-col text-center text-white sm:w-2/3 md:w-1/2 xl:w-1/2 ">
-                  <p className="">Popularne zabiegi</p>
+                <p className="">Popularne zabiegi</p>
                   <h3
-                    className={`${lato.className} text-lg sm:text-xl xl:text-2xl 2xl:text-4xl font-semibold rounded-xl mb-2`}
+                    className="text-lg sm:text-xl xl:text-2xl 2xl:text-4xl font-semibold rounded-xl mb-2"
                   >
                     {image.alt}
                   </h3>
                   <p
-                    className={`${lato.className} my-2  text-base sm:text-base lg:text-base xl:text-xl 2xl:text-2xl font-normal rounded-xl`}
+                    className=" my-2  text-base sm:text-base lg:text-base xl:text-xl 2xl:text-2xl font-normal rounded-xl`"
                   >
                     {image.description}
                   </p>
                   <div
-                    className={`my-2 flex justify-center gap-4 md:justify-center md:gap-4 `}
+                    className="my-2 flex justify-center gap-4 md:justify-center md:gap-4"
                   >
                     <Link
                       key={index}
