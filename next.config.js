dd6d368c4ee1d1657c9fc49c78957436f13f1e16
Piotr Regulski <bulu901@gmail.com>
2025-04-29 21:18:54 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true', // Włącz analizę tylko wtedy, gdy ANALYZE jest ustawione
-// });
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true', // Włącz analizę tylko wtedy, gdy ANALYZE jest ustawione
+});
 
-// module.exports = withBundleAnalyzer({
-//   reactStrictMode: true, // Twoja aktualna konfiguracja Next.js
-//   env: {
-//     HOMEPL_USER: process.env.HOMEPL_USER,
-//     HOMEPL_PASS: process.env.HOMEPL_PASS,
-//     CONTACT_FORM_TO_EMAIL: process.env.CONTACT_FORM_TO_EMAIL,
-//     SMTP_HOST: process.env.SMTP_HOST,
-//     SMTP_PORT: process.env.SMTP_PORT,
-//   },
-// });
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true, // Twoja aktualna konfiguracja Next.js
+  env: {
+    HOMEPL_USER: "kontakt+stanrelaksu_pl.serwer2422207",
+    HOMEPL_PASS: "Kin900%StanRelaksu",
+    CONTACT_FORM_TO_EMAIL: "kontakt@stanrelaksu.pl",
+    SMTP_HOST: "serwer2422207.home.pl",
+    SMTP_PORT: "25",
+  },
+});
