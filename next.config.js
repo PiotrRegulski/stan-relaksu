80cebb610ccd16a24187d3907f6db5dae3a66355
Piotr Regulski <bulu901@gmail.com>
2025-04-29 20:43:30 +0200

    chenged env

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   reactStrictMode: true, // Twoja aktualna konfiguracja Next.js
   env: {
-    HOMEPL_USER: "kontakt+stanrelaksu_pl.serwer2422207",
-    HOMEPL_PASS: "Kin900%StanRelaksu",
-    CONTACT_FORM_TO_EMAIL: "kontakt@stanrelaksu.pl",
-    SMTP_HOST: "serwer2422207.home.pl",
-    SMTP_PORT: "25",
+    HOMEPL_USER: process.env.HOMEPL_USER,
+    HOMEPL_PASS: process.env.HOMEPL_PASS,
+    CONTACT_FORM_TO_EMAIL: process.env.CONTACT_FORM_TO_EMAIL,
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
   },
 });
