654eef7f81fd25f030ab61c05de3f7ce2216a46c
Piotr Regulski <bulu901@gmail.com>
2025-04-29 20:59:18 +0200

    env

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,54 +1,61 @@
 import nodemailer from "nodemailer";
 import { NextRequest, NextResponse } from "next/server";
 
-
 interface CustomError extends Error {
   message: string;
 }
+
 export async function POST(req: NextRequest) {
-  const formData = await req.json();
-  const { name, email, subject, message } = formData;
-
-  if (!name || !email || !subject || !message) {
-    return new Response(
-      JSON.stringify({ message: "Wszystkie pola są wymagane." }),
-      { status: 400 }
-    );
-  }
-
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: 587, // Ustaw port na 25
-    secure: false, // Port 25 nie obsługuje TLS
-    auth: { user: process.env.HOMEPL_USER, pass: process.env.HOMEPL_PASS },
-    tls: {
-      minVersion: 'TLSv1.2', // Wymuś użycie TLS 1.2 lub nowszego
-    },
-    connectionTimeout: 10000, // 10 sekund
-  });
-
-  const mailOptions = {
-    from: `kontakt@stanrelaksu.pl`, // Użyj autoryzowanego adresu e-mail
-    replyTo: email, // Ustaw adres odpowiedzi na adres nadawcy
-    to: "kontakt@stanrelaksu.pl",
-    subject: `Wiadomość od ${name}: ${subject}`,
-    text: message,
-  };
-
   try {
+    const formData = await req.json();
+    const { name, email, subject, message } = formData;
+
+    if (!name || !email || !subject || !message) {
+      return NextResponse.json(
+        { message: "Wszystkie pola są wymagane." },
+        { status: 400 }
+      );
+    }
+
+    // Konfiguracja transportera Nodemailer
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT) || 587, // Pobieranie portu z env
+      secure: false,
+      auth: {
+        user: process.env.HOMEPL_USER,
+        pass: process.env.HOMEPL_PASS,
+      },
+      tls: {
+        minVersion: "TLSv1.2",
+      },
+      connectionTimeout: 10000,
+    });
+
+    // Opcje e-maila
+    const mailOptions = {
+      from: process.env.CONTACT_FORM_TO_EMAIL, // Pobranie autoryzowanego adresu z env
+      replyTo: email,
+      to: process.env.CONTACT_FORM_TO_EMAIL,
+      subject: `Wiadomość od ${name}: ${subject}`,
+      text: message,
+    };
+
     await transporter.sendMail(mailOptions);
-    return new Response(
-      JSON.stringify({ message: "Wiadomość wysłana pomyślnie!" }),
+
+    return NextResponse.json(
+      { message: "Wiadomość wysłana pomyślnie!" },
       { status: 200 }
     );
   } catch (error) {
     const customError = error as CustomError;
     console.error(customError);
-    return new Response(
-      JSON.stringify({
+
+    return NextResponse.json(
+      {
         message: "Wystąpił błąd podczas wysyłania wiadomości.",
         error: customError.message,
-      }),
+      },
       { status: 500 }
     );
   }
