2bd6fbcddd3f9e19fb111799d436878cd4cc4f46
Piotr Regulski <bulu901@gmail.com>
2025-05-06 19:44:04 +0200

    env fix

--- next.config.js
+++ next.config.js
@@ -5,11 +5,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true, // Twoja aktualna konfiguracja Next.js
-  env: {
-    HOMEPL_USER: "kontakt+stanrelaksu_pl.serwer2422207",
-    HOMEPL_PASS: "Kin900%StanRelaksu",
-    CONTACT_FORM_TO_EMAIL: "kontakt@stanrelaksu.pl",
-    SMTP_HOST: "serwer2422207.home.pl",
-    SMTP_PORT: "25",
-  },
+ 
 });
